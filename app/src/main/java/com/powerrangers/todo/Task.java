f237ea66bd114184c03f18fcafd249f43380c867
JoshuaNeely <joshua.a.neely@gmail.com>
2017-10-12 20:45:49 -0500

    Add a unique id to each task.

--- app/src/main/java/com/powerrangers/todo/Task.java
+++ app/src/main/java/com/powerrangers/todo/Task.java
@@ -2,13 +2,16 @@ package com.powerrangers.todo;
 
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.UUID;
 
 public class Task implements Serializable {
   public String name;
   public Calendar calendar;
+  public UUID id;
 
   public Task (String iname, Calendar icalendar) {
     name = iname;
     calendar = icalendar;
+    id = UUID.randomUUID();
   }
 }
