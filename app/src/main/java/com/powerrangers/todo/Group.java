d4c349e9e37b656c60f29ab0d774d964b6ce8ec7
bwmwf9 <bmissel68@gmail.com>
2017-10-17 12:14:35 -0500

    Added group tasks and members. Will update with all the functions once the user class is created.

--- app/src/main/java/com/powerrangers/todo/Group.java
+++ app/src/main/java/com/powerrangers/todo/Group.java
@@ -2,14 +2,21 @@ package com.powerrangers.todo;
 
 public class Group {
     public String group_name;
-    public String group_owner;
-    public int group_id;  //possibly use to allow for same names??
+    public String group_owner;  // change to User type
+    public int group_id;  // possibly use to allow for same names
+    public String[] group_members;  // will be changed to some collection of User type
+    public Task[] group_tasks;  // will be changed to some collection of Task type
 
     public Group (String name, String owner){
         group_name = name;
         group_owner = owner;
         group_id = 0;
     }
-    // getters and setters below
+
+    public boolean addTask(Task t){
+        // add task once in a collection not an array
+        return true;
+    }
+    //  more getters and setters below
 }
 
