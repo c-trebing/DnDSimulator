449707812c523bc3fd83e01b0cfe9e067d263a5c
DESKTOP-88JT2R1\jmkzp <jmkzpd@mst.edu>
2017-10-26 10:49:38 -0500

    fixed indenting

--- app/src/main/java/com/powerrangers/todo/LoginActivity.java
+++ app/src/main/java/com/powerrangers/todo/LoginActivity.java
@@ -28,130 +28,130 @@ import com.google.firebase.auth.GoogleAuthProvider;
  */
 public class LoginActivity extends AppCompatActivity {
 
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
+	/**
+	 * Id to identity READ_CONTACTS permission request.
+	 */
+	private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    //private UserLoginTask mAuthTask = null;
+	/**
+	 * Keep track of the login task to ensure we can cancel it if requested.
+	 */
+	//private UserLoginTask mAuthTask = null;
 
-    // UI references.
-    private SignInButton mGoogleBtn;
-    private GoogleApiClient mGoogleApiClient;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-    private static final String TAG = "Login_Activity";
-    private static final int RC_SIGN_IN = 1;
+	// UI references.
+	private SignInButton mGoogleBtn;
+	private GoogleApiClient mGoogleApiClient;
+	private FirebaseAuth mAuth;
+	private FirebaseAuth.AuthStateListener mAuthListener;
+	private static final String TAG = "Login_Activity";
+	private static final int RC_SIGN_IN = 1;
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
 
-        mGoogleBtn = (SignInButton) findViewById(R.id.googleBtn);
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
-                if (firebaseAuth.getCurrentUser() != null){
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                }
-            }
-        };
+		mGoogleBtn = (SignInButton) findViewById(R.id.googleBtn);
+		mAuth = FirebaseAuth.getInstance();
+		mAuthListener = new FirebaseAuth.AuthStateListener() {
+			@Override
+			public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+				if (firebaseAuth.getCurrentUser() != null){
+					startActivity(new Intent(LoginActivity.this, MainActivity.class));
+				}
+			}
+		};
 
-        // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
+		// Configure Google Sign In
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+				.requestIdToken(getString(R.string.default_web_client_id))
+				.requestEmail()
+				.build();
 
-        mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
-            .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
-                @Override
-                public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                    Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_LONG).show();
-                }
-            })
-            .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-            .build();
+		mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+			.enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+				@Override
+				public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+					Toast.makeText(LoginActivity.this, "Login Error", Toast.LENGTH_LONG).show();
+				}
+			})
+			.addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+			.build();
 
-        mGoogleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
-    }
+		mGoogleBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				signIn();
+			}
+		});
+	}
 
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
+	private void signIn() {
+		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+		startActivityForResult(signInIntent, RC_SIGN_IN);
+	}
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = result.getSignInAccount();
-                firebaseAuthWithGoogle(account);
-            } else {
-                // Google Sign In failed, update UI appropriately
-                Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG).show();
-                Log.w(TAG, "Error signing in. SignInAccount: " + result.getSignInAccount() + " Status:" + result.getStatus().toString());
-            }
-        }
-    }
+		// Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+		if (requestCode == RC_SIGN_IN) {
+			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+			if (result.isSuccess()) {
+				// Google Sign In was successful, authenticate with Firebase
+				GoogleSignInAccount account = result.getSignInAccount();
+				firebaseAuthWithGoogle(account);
+			} else {
+				// Google Sign In failed, update UI appropriately
+				Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG).show();
+				Log.w(TAG, "Error signing in. SignInAccount: " + result.getSignInAccount() + " Status:" + result.getStatus().toString());
+			}
+		}
+	}
 
-    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-        Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+	private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+		Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
 
-        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-        Log.d(TAG, "Credential: " + credential.toString());
-        mAuth.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> logintask) {
-                        if (logintask.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCredential:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCredential:failure", logintask.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-                    }
-                });
-    }
+		AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+		Log.d(TAG, "Credential: " + credential.toString());
+		mAuth.signInWithCredential(credential)
+				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+					@Override
+					public void onComplete(@NonNull Task<AuthResult> logintask) {
+						if (logintask.isSuccessful()) {
+							// Sign in success, update UI with the signed-in user's information
+							Log.d(TAG, "signInWithCredential:success");
+							FirebaseUser user = mAuth.getCurrentUser();
+							updateUI(user);
+						} else {
+							// If sign in fails, display a message to the user.
+							Log.w(TAG, "signInWithCredential:failure", logintask.getException());
+							Toast.makeText(LoginActivity.this, "Authentication failed.",
+									Toast.LENGTH_SHORT).show();
+							updateUI(null);
+						}
+					}
+				});
+	}
 
-    private void updateUI(FirebaseUser user) {
-        if (user != null) {
-            // User is signed in
-            Log.d(TAG, "Logged in and redirecting to MainActivity");
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivityForResult(intent, 100);
-        } else {
-            // No user is signed in
-            Log.d(TAG, "No user logged in");
-        }
-    }
+	private void updateUI(FirebaseUser user) {
+		if (user != null) {
+			// User is signed in
+			Log.d(TAG, "Logged in and redirecting to MainActivity");
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivityForResult(intent, 100);
+		} else {
+			// No user is signed in
+			Log.d(TAG, "No user logged in");
+		}
+	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        mAuth.addAuthStateListener(mAuthListener);
-        updateUI(currentUser);
-    }
+	@Override
+	public void onStart() {
+		super.onStart();
+		// Check if user is signed in (non-null) and update UI accordingly.
+		FirebaseUser currentUser = mAuth.getCurrentUser();
+		mAuth.addAuthStateListener(mAuthListener);
+		updateUI(currentUser);
+	}
 }
--- app/src/main/java/com/powerrangers/todo/MainActivity.java
+++ app/src/main/java/com/powerrangers/todo/MainActivity.java
@@ -29,200 +29,200 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+		implements NavigationView.OnNavigationItemSelectedListener {
 
-    ArrayList<TaskActivity> tasks;
+	ArrayList<TaskActivity> tasks;
 
-    MyExpandableListAdapter listAdaptor;
-    ExpandableListView listView;
-    List<String> listDataHeaders;
-    HashMap<String, List<String>> listDataChildren;
-    private static final String TAG = "Main_Activity";
-    private FirebaseAuth mAuth;
+	MyExpandableListAdapter listAdaptor;
+	ExpandableListView listView;
+	List<String> listDataHeaders;
+	HashMap<String, List<String>> listDataChildren;
+	private static final String TAG = "Main_Activity";
+	private FirebaseAuth mAuth;
 
-    /*********test of firebase********/
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    /*********test of firebase********/
+	/*********test of firebase********/
+	FirebaseDatabase database = FirebaseDatabase.getInstance();
+	/*********test of firebase********/
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.create_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Snackbar.make(view, "Replace this text with a tray of buttons...", Snackbar.LENGTH_LONG)
-                //         .setAction("Action", null).show();
-                Context context = view.getContext();
-                Intent intent = new Intent(context, CreateTaskActivity.class);
-                startActivityForResult(intent, 100);
-            }
-        });
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.create_fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				// Snackbar.make(view, "Replace this text with a tray of buttons...", Snackbar.LENGTH_LONG)
+				//         .setAction("Action", null).show();
+				Context context = view.getContext();
+				Intent intent = new Intent(context, CreateTaskActivity.class);
+				startActivityForResult(intent, 100);
+			}
+		});
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+		drawer.setDrawerListener(toggle);
+		toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+		navigationView.setNavigationItemSelectedListener(this);
 
-        tasks = new ArrayList<TaskActivity>();
-        listDataHeaders = new ArrayList<String>();
-        listDataChildren = new HashMap<String, List<String>>();
+		tasks = new ArrayList<TaskActivity>();
+		listDataHeaders = new ArrayList<String>();
+		listDataChildren = new HashMap<String, List<String>>();
 
-        listAdaptor = new MyExpandableListAdapter(this, listDataHeaders, listDataChildren);
-        listView = (ExpandableListView) findViewById(R.id.task_list);
-        listView.setAdapter(listAdaptor);
-        mAuth = FirebaseAuth.getInstance();
+		listAdaptor = new MyExpandableListAdapter(this, listDataHeaders, listDataChildren);
+		listView = (ExpandableListView) findViewById(R.id.task_list);
+		listView.setAdapter(listAdaptor);
+		mAuth = FirebaseAuth.getInstance();
 
-        prepareMockData();
-    }
+		prepareMockData();
+	}
 
-    @Override
-    public void onResume () {
-        super.onResume();
+	@Override
+	public void onResume () {
+		super.onResume();
 
-        // expand groups by default
-        int count = listAdaptor.getGroupCount();
-        for (int i=0; i<count; i++) {
-          listView.expandGroup(i);
-        }
-    }
+		// expand groups by default
+		int count = listAdaptor.getGroupCount();
+		for (int i=0; i<count; i++) {
+		  listView.expandGroup(i);
+		}
+	}
 
-    @Override
-    public void onActivityResult (int requestCode, int resultCode, Intent intent) {
-      if (resultCode == 200 && requestCode == 100) {
-          TaskActivity task = (TaskActivity)intent.getSerializableExtra("CREATED_TASK");
-        addTask(task);
-        sortTasks();
-        updateDisplayedTasks();
+	@Override
+	public void onActivityResult (int requestCode, int resultCode, Intent intent) {
+	  if (resultCode == 200 && requestCode == 100) {
+		  TaskActivity task = (TaskActivity)intent.getSerializableExtra("CREATED_TASK");
+		addTask(task);
+		sortTasks();
+		updateDisplayedTasks();
 
-      }
-    }
+	  }
+	}
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+	@Override
+	public void onBackPressed() {
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+		} else {
+			super.onBackPressed();
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        }
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			Intent intent = new Intent(this, SettingsActivity.class);
+			startActivity(intent);
+			return true;
+		}
 
-        return super.onOptionsItemSelected(item);
-    }
+		return super.onOptionsItemSelected(item);
+	}
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+	@SuppressWarnings("StatementWithEmptyBody")
+	@Override
+	public boolean onNavigationItemSelected(MenuItem item) {
+		// Handle navigation view item clicks here.
+		int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+		if (id == R.id.nav_camera) {
+			// Handle the camera action
+		} else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+		} else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+		} else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
+		} else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_send) {
+		} else if (id == R.id.nav_send) {
 
-        } else if (id == R.id.nav_logout) {
-            Log.d(TAG, "Logging out");
-            mAuth.signOut();
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivityForResult(intent, 100);
-        }
+		} else if (id == R.id.nav_logout) {
+			Log.d(TAG, "Logging out");
+			mAuth.signOut();
+			Intent intent = new Intent(this, LoginActivity.class);
+			startActivityForResult(intent, 100);
+		}
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawer.closeDrawer(GravityCompat.START);
+		return true;
+	}
 
-    private void addTask (TaskActivity task) {
-        tasks.add( task );
+	private void addTask (TaskActivity task) {
+		tasks.add( task );
 
-        /** Update Firebase with new information upon addTask**/
-        SimpleDateFormat taskFormat = new SimpleDateFormat("EEEE, MMM d @ hh:mm a  -  ");
-        String header = taskFormat.format(task.calendar.getTime());
-        DatabaseReference myRef = database.getReference(header);
-        myRef.setValue(task.name);
-    }
+		/** Update Firebase with new information upon addTask**/
+		SimpleDateFormat taskFormat = new SimpleDateFormat("EEEE, MMM d @ hh:mm a  -  ");
+		String header = taskFormat.format(task.calendar.getTime());
+		DatabaseReference myRef = database.getReference(header);
+		myRef.setValue(task.name);
+	}
 
-    private void sortTasks () {
-      Collections.sort(tasks, new Comparator<TaskActivity>() {
-        @Override
-        public int compare(TaskActivity t1, TaskActivity t2) {
-          return t1.calendar.getTime().compareTo(t2.calendar.getTime());
-        }
-      });
-    }
+	private void sortTasks () {
+	  Collections.sort(tasks, new Comparator<TaskActivity>() {
+		@Override
+		public int compare(TaskActivity t1, TaskActivity t2) {
+		  return t1.calendar.getTime().compareTo(t2.calendar.getTime());
+		}
+	  });
+	}
 
-    private void updateDisplayedTasks () {
-      SimpleDateFormat taskHeaderFormat = new SimpleDateFormat("EEEE, MMM d");
-      SimpleDateFormat taskEntryFormat = new SimpleDateFormat("hh:mm a  -  ");
+	private void updateDisplayedTasks () {
+	  SimpleDateFormat taskHeaderFormat = new SimpleDateFormat("EEEE, MMM d");
+	  SimpleDateFormat taskEntryFormat = new SimpleDateFormat("hh:mm a  -  ");
 
-      // Kinda naive to tear it down and rebuild it at every change,
-      // but listAdaptors are funky to work with. It works.
-      listDataHeaders.clear();
-      listDataChildren.clear();
+	  // Kinda naive to tear it down and rebuild it at every change,
+	  // but listAdaptors are funky to work with. It works.
+	  listDataHeaders.clear();
+	  listDataChildren.clear();
 
-      for (TaskActivity task : tasks) {
-        String header = taskHeaderFormat.format(task.calendar.getTime());
-        String entry = taskEntryFormat.format(task.calendar.getTime()) + task.name;
-        // if it doesnt exist, create it
-        if (listDataHeaders.indexOf(header) == -1) {
-          listDataHeaders.add(header);
-          listDataChildren.put(header, new ArrayList<String>());
-        }
+	  for (TaskActivity task : tasks) {
+		String header = taskHeaderFormat.format(task.calendar.getTime());
+		String entry = taskEntryFormat.format(task.calendar.getTime()) + task.name;
+		// if it doesnt exist, create it
+		if (listDataHeaders.indexOf(header) == -1) {
+		  listDataHeaders.add(header);
+		  listDataChildren.put(header, new ArrayList<String>());
+		}
 
-        listDataChildren.get(header).add(entry);
-        listAdaptor.setNewItems(listDataHeaders, listDataChildren);
-      }
-    }
+		listDataChildren.get(header).add(entry);
+		listAdaptor.setNewItems(listDataHeaders, listDataChildren);
+	  }
+	}
 
-    private void prepareMockData () {
-      Calendar today = Calendar.getInstance();
-      Calendar tomorrow = Calendar.getInstance();
-      tomorrow.add(Calendar.DATE, 1);
+	private void prepareMockData () {
+	  Calendar today = Calendar.getInstance();
+	  Calendar tomorrow = Calendar.getInstance();
+	  tomorrow.add(Calendar.DATE, 1);
 
-      for (int i=1; i<20; i++) {
-        addTask( new TaskActivity("problem " + i, tomorrow) );
-      }
-      addTask( new TaskActivity("panic about tomorrow", today) );
+	  for (int i=1; i<20; i++) {
+		addTask( new TaskActivity("problem " + i, tomorrow) );
+	  }
+	  addTask( new TaskActivity("panic about tomorrow", today) );
 
-      sortTasks();
-      updateDisplayedTasks();
-    }
+	  sortTasks();
+	  updateDisplayedTasks();
+	}
 }
